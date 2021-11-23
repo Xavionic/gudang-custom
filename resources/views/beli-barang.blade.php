c31096983f78d8b22b35a74e8b3489451bb88edd
Xavionic <dotasabili@gmail.com>
2021-11-24 00:34:55 +0700

    Percobaan Push Github

--- resources/views/beli-barang.blade.php
+++ resources/views/beli-barang.blade.php
@@ -3,6 +3,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 {{-- <x-jet-welcome /> --}}
+                {{-- test github --}}
+
             </div>
         </div>
     </div>
